555bb3c087a7489de6afc2b369ac1fa3caa59cdb
byol-han <byolhandev@gmail.com>
2025-07-11 21:07:06 -0700

    webtoon homescreen done

--- lib/screens/webtoon_screen.dart
+++ lib/screens/webtoon_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/services/api_service.dart';
 class WebtoonScreen extends StatelessWidget {
   WebtoonScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService().getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService().getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -24,11 +24,61 @@ class WebtoonScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text('There is data');
+            // return ListView.builder(
+            //   scrollDirection: Axis.horizontal,
+            //   itemCount: snapshot.data!.length,
+            //   itemBuilder: (context, index) {
+            //     var webtoon = snapshot.data![index];
+
+            //     return Text(webtoon.title);
+            //   },
+            // );
+            return Column(
+              children: [
+                SizedBox(height: 50),
+                Expanded(child: makeList(snapshot)),
+              ],
+            );
           }
-          return const Text('Loading...');
+          return Center(child: CircularProgressIndicator());
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 10,
+                    offset: Offset(10, 10),
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {'Referer': 'https://comic.naver.com'},
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(webtoon.title, style: TextStyle(fontSize: 22)),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(width: 40),
     );
   }
 }
